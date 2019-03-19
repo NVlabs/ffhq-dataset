a0fbb8418d53494c1b97d191ba6c8bf12f577d06
Tero Karras <tkarras@nvidia.com>
2019-03-19 13:11:16 +0200

    Search tool.

---
+++ docs/search/precache-manifest.d5e29510a2069c4ce4aaafff8ab19b02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "094ecdea28debf254d73",
+    "url": "/ffhq-dataset/search/static/js/runtime~main.e5de1b36.js"
+  },
+  {
+    "revision": "a8f2095efa3f224440e1",
+    "url": "/ffhq-dataset/search/static/js/main.3ccdce75.chunk.js"
+  },
+  {
+    "revision": "a48068256375f306d831",
+    "url": "/ffhq-dataset/search/static/js/2.f9cc10ee.chunk.js"
+  },
+  {
+    "revision": "a8f2095efa3f224440e1",
+    "url": "/ffhq-dataset/search/static/css/main.48ff51cb.chunk.css"
+  },
+  {
+    "revision": "8fa34a93b159b02845a3a8198209fc1e",
+    "url": "/ffhq-dataset/search/index.html"
+  }
+];
---
+++ docs/search/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/ffhq-dataset/search/precache-manifest.d5e29510a2069c4ce4aaafff8ab19b02.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/ffhq-dataset/search/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
